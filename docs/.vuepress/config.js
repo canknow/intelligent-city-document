4e9fdb260974fcf6f617c5b81021495f6f34b38b
canknow <3230525823@qq.com>
2023-04-08 04:03:43 +0800

    perf: no message

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ import { defaultTheme } from 'vuepress'
 import { nprogressPlugin } from '@vuepress/plugin-nprogress'
 import { backToTopPlugin } from '@vuepress/plugin-back-to-top'
 import { mediumZoomPlugin } from '@vuepress/plugin-medium-zoom'
+import { searchPlugin } from '@vuepress/plugin-search'
 
 export default {
     base: '/intelligent-city-document/',
@@ -9,19 +10,25 @@ export default {
     description: '智慧城市官方文档',
     themeConfig: {
         search: true,
-        searchMaxSuggestions: 10,
-        lastUpdated: '最后更新于',
+        searchMaxSuggestions: 10
     },
     theme: defaultTheme({
         logo: '/logo.png',
+        colorModeSwitch: true,
+        lastUpdatedText: '最后更新于',
+        contributorsText: '贡献值',
+        sidebar: 'auto',
         navbar: [
             { text: '首页', link: '/' },
             { text: '简介', link: '/introduce' },
-            { text: '系统功能', link: '/functions' },
+            { text: '系统功能', children: ['/functions/admin/','/functions/user/','/functions/business/'] },
             { text: '技术架构', link: '/technicalArchitecture' },
         ],
     }),
     plugins: [
+        searchPlugin({
+            // 配置项
+        }),
         backToTopPlugin(),
         nprogressPlugin(),
         mediumZoomPlugin({
